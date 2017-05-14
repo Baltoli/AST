544f019d65fef53d87ad059f9e740a43c86f33c2
Bruce Collie <bsc28@cam.ac.uk>
2017-05-14 15:18:19 +0100

    Implement syntax checker

--- src/app/src/astc.cpp
+++ src/app/src/astc.cpp
@@ -2,8 +2,24 @@
 #include <ast/parser.h>
 
 #include <iostream>
+#include <sstream>
 
-int main()
+int main(int argc, char **argv)
 {
+  std::stringstream ss;
+
+  for(std::string line; std::getline(std::cin, line); ) {
+    ss << line << '\n';
+  }
+
+  auto result = ast::CompositeParser(ss.str()).get();
+
+  if(result) {
+    std::cout << result.data->str() << '\n';
+  } else {
+    std::cout << "Syntax error!\n";
+    return 1;
+  }
+
   return 0;
 }
