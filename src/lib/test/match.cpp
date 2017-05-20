fcc3ef15061583e99f6129a1e3f770aae2536eb0
Bruce Collie <bsc28@cam.ac.uk>
2017-05-20 12:02:41 +0100

    Use new reference matching in tests

--- src/lib/test/match.cpp
+++ src/lib/test/match.cpp
@@ -2,78 +2,66 @@
 
 #include "catch.h"
 
+using namespace ast;
+
 TEST_CASE("any matches") {
-  auto a = ast::Any();
+  auto a = Any();
 
-  auto s = std::make_unique<ast::Symbol>("dj");
-  REQUIRE(a.match(ast::Symbol("dj")));
-
-  auto c = std::make_unique<ast::Composite>();
-  REQUIRE(a.match(*c));
+  REQUIRE(a.match(Symbol("dj")));
+  REQUIRE(a.match(Composite()));
 }
 
 TEST_CASE("exact matches") {
-  auto ex = ast::Exact(ast::Symbol("sym"));
+  auto ex = Exact(Symbol("sym"));
 
-  auto s = std::make_unique<ast::Symbol>("sym");
-  REQUIRE(ex.match(*s));
+  REQUIRE(ex.match(Symbol("sym")));
+  REQUIRE(!ex.match(Symbol("jefw")));
 
-  auto t = std::make_unique<ast::Symbol>("jefw");
-  REQUIRE(!ex.match(*t));
-
-  auto c = ast::Composite();
-  c.add_member(ast::Symbol("sym"));
+  auto c = Composite();
+  c.add_member(Symbol("sym"));
   REQUIRE(!ex.match(c));
 }
 
 TEST_CASE("either matches") {
-  auto a = std::make_unique<ast::Exact>(ast::Symbol("a"));
-  auto b = std::make_unique<ast::Exact>(ast::Symbol("b"));
-  auto e = ast::Either(a.get(), b.get());
+  auto a = std::make_unique<Exact>(Symbol("a"));
+  auto b = std::make_unique<Exact>(Symbol("b"));
+  auto e = Either(a.get(), b.get());
 
-  auto s = std::make_unique<ast::Symbol>("a");
-  REQUIRE(e.match(*s));
-
-  auto t = std::make_unique<ast::Symbol>("b");
-  REQUIRE(e.match(*t));
-
-  auto u = std::make_unique<ast::Symbol>("c");
-  REQUIRE(!e.match(*u));
+  REQUIRE(e.match(Symbol("a")));
+  REQUIRE(e.match(Symbol("b")));
+  REQUIRE(!e.match(Symbol("c")));
 }
 
 TEST_CASE("both matches") {
-  auto a = ast::Exact(ast::Symbol("a"));
-  auto ca = ast::HasChild(&a);
+  auto a = Exact(Symbol("a"));
+  auto ca = HasChild(&a);
 
-  auto b = ast::Exact(ast::Symbol("b"));
-  auto cb = ast::HasChild(&b);
+  auto b = Exact(Symbol("b"));
+  auto cb = HasChild(&b);
 
-  auto c = ast::Both(&ca, &cb);
+  auto c = Both(&ca, &cb);
 
-  auto comp = ast::Composite();
-  comp.add_member(ast::Symbol("a"));
+  auto comp = Composite();
+  comp.add_member(Symbol("a"));
   REQUIRE(!c.match(comp));
 
-  comp.add_member(ast::Symbol("b"));
+  comp.add_member(Symbol("b"));
   REQUIRE(c.match(comp));
 }
 
 TEST_CASE("has child matches") {
-  auto ch = ast::Exact(ast::Symbol("sym"));
-  auto ex = ast::HasChild(&ch);
+  auto ch = Exact(Symbol("sym"));
+  auto ex = HasChild(&ch);
 
-  auto c = ast::Composite();
-  c.add_member(ast::Symbol("sym"));
+  auto c = Composite();
+  c.add_member(Symbol("sym"));
   REQUIRE(ex.match(c));
 
-  auto d = ast::Composite();
-  REQUIRE(!ex.match(d));
-  
-  auto e = ast::Symbol("sym");
-  REQUIRE(!ex.match(e));
+  REQUIRE(!ex.match(Composite()));
+  REQUIRE(!ex.match(Symbol("sym")));
 
-  auto f = ast::Composite();
-  f.add_member(ast::Symbol("ewoi"));
-  f.add_member(ast::Symbol("sym"));
+  auto f = Composite();
+  f.add_member(Symbol("ewoi"));
+  f.add_member(Symbol("sym"));
   REQUIRE(ex.match(f));
 }
